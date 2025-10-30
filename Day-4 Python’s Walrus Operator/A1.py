018aed2044615163363e982432fcffbbbecd41f4
vikaschauhan <vikaspravinbhai@gmail.com>
2025-10-30 15:13:14 +0530

    The Use Of Walrus operator in python

---
+++ Day-4 Python’s Walrus Operator/A1.py
@@ -0,0 +1,20 @@
+"""
+data =input("Enter Some Data")
+while data!='quit':
+    data =input("Enter Some Data")
+    print("Enter Data : ", data)
+    
+"""
+
+#python are popular for presize coding.
+# we can use of than the uper hole code in 2 line
+#By the use of Walrus Operator 
+
+while (data:=input("Eneter some data"))!='quit':
+    print("Enterd Data",data)
+
+
+
+#AT the end of code we learn if we want to run 2 comand in one line 
+#Than use Walrus Operator ":=" thsi is Wakrus Operartoer 
+# the use of walrus oprator we can make small line od python code
