4b6f45862a93ffcce883d496dc2d479f80284f23
vikaschuahan <vikasparvinbhai@gmail.com>
2025-11-14 23:39:30 +0530

    Set Data Structure in JS

--- Day-19 Set Data Structure in JavaScript/CustomSet.js
+++ Day-19 Set Data Structure in JavaScript/CustomSet.js
@@ -1,74 +1,75 @@
- class CustomeSet{
-    Constructor(){
-        this.iteam= new Map();
-
+class CustomSet {
+    constructor() {
+        this.items = new Map();
     }
-    add(element){
-        if(this.iteams.has(element)){
+
+    add(element) {
+        if (this.items.has(element)) {
             return false;
         }
-        this.iteams.set(element,element);
+        this.items.set(element, element);
         return true;
-        }
+    }
 
-        has(element){
-            return this.iteams.has(element);
-        }
-        remove(element){
-            return this.iteams.delete(element);
-        }
-        size(){
-            return  this.iteams.size;
+    has(element) {
+        return this.items.has(element);
+    }
 
-        }
-        values(){
-            return[...this.iteams.keys()];
+    remove(element) {
+        return this.items.delete(element);
+    }
 
-        }
-        union(otherSet){
-            const unionSet=new CustomeSet();
-            this.values().forEach(value => unionSet.add(value));
+    size() {
+        return this.items.size;
+    }
 
-            otherSet.value.forEach(value => unionSet.add
-                (value));
-                return unionSet;
-        }
+    values() {
+        return [...this.items.keys()];
+    }
 
-        IntersectionObserver(otherSet){
-            const intersectionSet=new CustomeSet();
-            
-            const samllerSet=this.size() <otherSet.size()?
-            this:otherSet;
-            const largerSet=this.size() <otherSet.size()?
-            otherSet:this;
+    union(otherSet) {
+        const unionSet = new CustomSet();
+        this.values().forEach(value => unionSet.add(value));
+        otherSet.values().forEach(value => unionSet.add(value));
+        return unionSet;
+    }
 
+    intersection(otherSet) {
+        const intersectionSet = new CustomSet();
 
-            samllerSet.value().forEach(value =>{
-                if(largerSet.has(value)){
-                    intersectionSet.add(value);
-                }
-            });
-            return intersectionSet;
-        }
- }
+        const smallerSet = this.size() < otherSet.size() ? this : otherSet;
+        const largerSet = this.size() < otherSet.size() ? otherSet : this;
 
-const setA=new CustomeSet();
+        smallerSet.values().forEach(value => {
+            if (largerSet.has(value)) {
+                intersectionSet.add(value);
+            }
+        });
+
+        return intersectionSet;
+    }
+}
+
+const setA = new CustomSet();
 setA.add(2);
 setA.add(4);
 setA.add(6);
 setA.add(8);
 setA.add(10);
-const setB=new CustomeSet();
+
+const setB = new CustomSet();
 setB.add(1);
 setB.add(4);
 setB.add(7);
 setB.add(8);
-console.log(setA);
-console.log(SetB);
 
-setC=setA.union(setB);
+console.log(setA);
+console.log(setB);
+
+const setC = setA.union(setB);
 console.log(setC);
-setD=setA.intersection(setB);
+
+const setD = setA.intersection(setB);
 console.log(setD);
 
 setA.remove(2);
