ebe7b51b17250efc8d1c7f5a0ff44f869e620c78
vikaschuahan <vikasparvinbhai@gmail.com>
2025-11-20 00:02:29 +0530

    XML Reading throw the Python

--- Day-23 handle XML through Python/XMLReader.py
+++ Day-23 handle XML through Python/XMLReader.py
@@ -3,7 +3,9 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('employee.xml')
 root = tree.getroot()
 
-print("Name | Age | Gender | Email | Phone | Address")
+print("Name | Age | Gender | Email | Phone | Address | Status")
+print("-" * 70)
+
 for emp in root.findall('employee'):
     name = emp.find('name').text
     age = emp.find('age').text
@@ -11,5 +13,6 @@ for emp in root.findall('employee'):
     email = emp.find('email').text
     phone = emp.find('phone').text
     address = emp.find('address').text
+    status = emp.get('status')  # Get attribute value
     
-    print('{} | {} | {} | {} | {} | {}'.format(name, age, gender, email, phone, address))
+    print('{} | {} | {} | {} | {} | {} | {}'.format(name, age, gender, email, phone, address, status))
