d9b3ea37c12b82a40b2d04cb133181ff54ebf183
vikaschuahan <vikasparvinbhai@gmail.com>
2025-11-21 23:14:16 +0530

    How To make Class Iterable in python

---
+++ Day-25 How to Make Your Class Iterable in Python/a1.py
@@ -0,0 +1,32 @@
+class Counter:
+    def __init__(self, start=1, end=10):
+        self.start = start
+        self.end = end
+    
+    def __iter__(self):
+        return self.Counter_Iterator(self)
+    
+    class Counter_Iterator:
+        def __init__(self, counter):
+            self.counter = counter
+            self.beg = counter.start
+        
+        def __iter__(self):
+            return self
+        
+        def __next__(self):
+            if self.beg > self.counter.end:
+                raise StopIteration
+            current_value = self.beg
+            self.beg += 1
+            return current_value
+
+        
+my_counter = Counter(1, 5)        
+
+for i in my_counter:
+    print(i)
+
+print("\nIterating again:")
+for i in my_counter:
+    print(i)
